9e861afe087ebd6d94cf8960711dc9b78899f97c
OLUTUYO PELUMI <knathgraphics@gmail.com>
2023-03-14 09:34:12 -0700

    Changed Height to 50 final change

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -291,7 +291,7 @@ class _DashboardState extends State<Dashboard> {
                 textAlign: TextAlign.justify,
               ),
               SizedBox(
-                height: 546,
+                height: 50,
               ),
             ],
           ),
